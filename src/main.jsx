17a05f5b63d7f86485691c5f595f72fc99f1662e
Syed Mir Habib <mirakik.habib@gmail.com>
2023-04-13 00:44:34 +0600

    router

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter ([
+{
+    path:'/',
+    element: <App/>,
+}
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
